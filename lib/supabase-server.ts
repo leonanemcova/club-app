762a75dff8c9eb6acb1ba9a13b51e1707e152f06
leonanemcova <leona.n.97@seznam.cz>
2026-04-23 16:49:05 +0200

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -10,7 +10,7 @@ export function createServerSupabase() {
     {
       cookies: {
         getAll() { return cookieStore.getAll() },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)

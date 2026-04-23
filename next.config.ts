85b3e12efa240634d34df6e910ecd773aa67dab6
leonanemcova <leona.n.97@seznam.cz>
2026-04-23 16:42:57 +0200

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-// next.config.ts
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  experimental: { serverActions: { allowedOrigins: ['*'] } },
-}
-
-export default nextConfig
